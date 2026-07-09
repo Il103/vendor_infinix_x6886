1184739bcb73e2ca9aea40e23ed1f93cf4cff0cb
Il103 <looooly590@gmail.com>
2026-07-09 21:15:23 +0000

    vendor/x6886: Regenerate proprietary-files.txt and BoardConfigVendor.mk
    - proprietary-files.txt: regenerated with 4180 entries matching actual blobs
    - BoardConfigVendor.mk: recreated (modules handled by kernel tree)
    - Track current subset of proprietary blobs (4180 files on disk)

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,10 @@
 #
-# Automatically generated file. DO NOT MODIFY
+# Copyright (C) 2024-2025 The LineageOS Project
 #
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Kernel modules are handled by the kernel tree:
+# - Ramdisk modules: kernel/infinix/x6886/ramdisk/*.ko
+# - Vendor DLKM modules: kernel/infinix/x6886/vendor_dlkm/*.ko
+# - Configured in BoardConfig.mk
